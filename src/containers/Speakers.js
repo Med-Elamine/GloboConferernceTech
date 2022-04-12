5e31d3c29d4ebd405a0de4eddb45b7ecdfeceaff
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-12 16:10:22 +0000

    Add a Search Input to the list of speakers

--- src/containers/Speakers.js
+++ src/containers/Speakers.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import {Text, View, FlatList, SafeAreaView, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, FlatList, SafeAreaView, Image, TextInput} from 'react-native';
 import {speakers} from '../data/speakers.json';
 import styles from '../containers/styles/sharedStyles';
 import Header  from '../components/Header';
@@ -12,6 +12,7 @@ function Speakers() {
         <View style={styles.sectionContainer} key={props.id}>
             <Text style={styles.sectionTitle}>Speaker Name : {props.name}</Text>
             <Text style={styles.sectionDescription}>Speaker Bio : {props.bio}</Text>
+            <Text style={styles.sectionDescription}>Session Name : {props.session}</Text>
         </View>
         );
     };
@@ -19,7 +20,7 @@ function Speakers() {
     const speakerItem = ({item, index}) => {
         return (
             <View>
-                <SpeakerList id={index} name={item.name} bio={item.bio} />
+                <SpeakerList id={index} name={item.name} bio={item.bio} session={item.sessions[0].name} />
             </View>
         );
     };
@@ -44,8 +45,38 @@ function Speakers() {
         </View>);
     };
 
+
+
+    const SearchSessions = (props) => {
+        const [searchText, setSearchText] = useState('');
+
+        const handleSearch = (text) => {
+            setSearchText(text);
+            props.getSearchText(text);
+        };
+
+        return(
+            <View style={styles.container}>
+                <TextInput 
+                    style={styles.searchInput}
+                    value={searchText}
+                    onChangeText={(text) => handleSearch(text)}
+                />
+            </View>
+        );
+    };
+
+    const [filteredSpeakers, setFilteredSpeakers] = useState(speakers);
+
+    const getSearchText = (text) => {
+        let filteredSpeakersList = filteredSpeakers.filter((value) => 
+        value.sessions[0].name.toLowerCase().includes(text.toLowerCase()));
+        setFilteredSpeakers(filteredSpeakersList);
+    }
+
   return (
       <SafeAreaView>
+        <SearchSessions getSearchText={getSearchText}/>
         <FlatList
         data={speakers}
         keyExtractor={(item) => item.id}
