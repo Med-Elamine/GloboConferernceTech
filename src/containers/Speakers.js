52f9e3d6464d7ee3c7a74af176e68d04901d8fe4
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-12 10:20:30 +0000

    Rendring Large Lists with FlatList Component (Separtor, header & footer compo)

--- src/containers/Speakers.js
+++ src/containers/Speakers.js
@@ -1,9 +1,62 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, FlatList, SafeAreaView, Image} from 'react-native';
+import {speakers} from '../data/speakers.json';
+import styles from '../containers/styles/sharedStyles';
 
 function Speakers() {
-  return <Text>Display speakers here</Text>;
+
+    const SpeakerList = (props) => {
+        return (
+        <View style={styles.sectionContainer} key={props.id}>
+            <Text style={styles.sectionTitle}>Speaker Name : {props.name}</Text>
+            <Text style={styles.sectionDescription}>Speaker Bio : {props.bio}</Text>
+        </View>
+        );
+    };
+
+    const speakerItem = ({item, index}) => {
+        return (
+            <View>
+                <SpeakerList id={index} name={item.name} bio={item.bio} />
+            </View>
+        );
+    };
+
+    const SeparatorCompo = () => {
+        return <View style={styles.separatorStyle} />;
+    };
+
+    const HeaderCompo = () => {
+        return (
+        <View style={styles.sectionContainer}>
+            <Image style= {styles.headerCompo}
+            source={require('../images/girl.png')}/>
+            <Text style={styles.sectionDescription}>Speakers Lineup!!</Text>
+        </View>);
+    };
+
+    const FooterCompo = () => {
+        return (
+        <View style={styles.sectionContainer}>
+            <Image style= {styles.headerCompo}
+            source={require('../images/ytb.png')}/>
+            <Text style={styles.sectionDescription}>All right reserved by aminel</Text>
+        </View>);
+    };
+
+  return (
+      <SafeAreaView>
+        <FlatList
+        data={speakers}
+        keyExtractor={(item) => item.id}
+        renderItem={speakerItem}
+        ItemSeparatorComponent={SeparatorCompo}
+        ListHeaderComponent={HeaderCompo}
+        ListFooterComponent={FooterCompo}
+        />
+      </SafeAreaView>
+  );
 }
 
 export default Speakers;
--- src/containers/styles/sharedStyles.js
+++ src/containers/styles/sharedStyles.js
@@ -1,5 +1,16 @@
 import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
+  separatorStyle: {
+    height: 2,
+    backgroundColor: 'red',
+    paddingTop: 2,
+    marginTop: 25,
+  },
+  headerCompo: {
+    width: '100%',
+    height: 250,
+    resizeMode: 'cover',
+  },
   imagehome: {
     width: 180,
     height: 100,
